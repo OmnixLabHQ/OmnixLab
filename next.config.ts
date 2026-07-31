376800bf3a9f40bf992916461b3d8135a9ba7b17
Gold Trading Coach <apps@heliumit.net>
2026-07-31 18:03:26 +0100

    Add cache headers for better GTmetrix performance

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,28 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|webp|avif)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:all*(js|css)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
